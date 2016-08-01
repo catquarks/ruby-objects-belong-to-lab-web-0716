510396423a4857b5021631dd6dfdc5c1676f1df9
Tami <catquarks@gmail.com>
2016-07-31 23:18:30 -0400

    defined classes that can be related via the 'belongs to' relationship

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist
+	attr_accessor :name, :genre
+
+	# def initialize(name, genre)
+	# 	@name = name
+	# 	@genre = genre
+	# end
+
+end
+
+# the_monkees = Artist.new("The Monkees", "psychedelic")
+# puts the_monkees.name
+# puts the_monkees.genre
